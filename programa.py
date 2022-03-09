96323d9b9aec8c3e95e066d0a7bdfa08f6109048
Diego Alves <inboxdgo@gmail.com>
2022-03-09 18:07:20 -0300

    Create programa.py

---
+++ programa.py
@@ -0,0 +1,4 @@
+print("computador") # Escreve 'Computador'
+x = 40              # x recebe 40. Agora existe uma variável chamada x que vale 40.
+y = x*234           # y recebe x*234
+print(y)            # Escreve y
