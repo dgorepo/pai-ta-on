e01cd8989be33dc4af33cf36abc241dfe1c1e7f2
Diego Alves <inboxdgo@gmail.com>
2022-03-11 07:41:13 -0300

    Create 03_tipos_variaveis_a.py

---
+++ 03_tipos_variaveis_a.py
@@ -0,0 +1,8 @@
+# Não é necessário declarar o tipo de variável, o interpretador define pelo contexto.
+
+x = 150                 # Inteiro
+y = 2.3                 # Float 
+z = "Eu amo Python"     # String
+u = "150"               # String 
+
+print(x, y, z)          # Apresenta o valor das variáveis na tela.
