833bd13f485d18d654655bd85a47e2e2da59bfee
Diego Alves <inboxdgo@gmail.com>
2022-03-11 07:42:44 -0300

    Update 03_tipos_variaveis.py

--- 03_tipos_variaveis.py
+++ 03_tipos_variaveis.py
@@ -6,3 +6,26 @@ z = "Eu amo Python"     # String
 u = "150"               # String 
 
 print(x, y, z)          # Apresenta o valor das variáveis na tela.
+
+# Operações básicas
+
+x = 10*20 + 30          
+
+y = 2/3 - 2**3          # O operador ** significa potência.
+
+print(10//3)
+
+# Usando variáveis para definir novas variáveis
+
+x = 10
+y = 20
+z = x + y + x*y
+
+print(z)
+
+# Você pode recuperar o tipo de variável
+
+x = 10
+y = "Relógio"
+z = 1.2
+print(type(x), type(y), type(z))
